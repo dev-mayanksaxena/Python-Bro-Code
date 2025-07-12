b97eb9942b8197b21c02755033e3c7c355129f76
Mayank Saxena <dev.mayanksaxena@gmail.com>
2025-07-12 15:42:15 +0530

    Madlibs Game

--- madlibs_game.py
+++ madlibs_game.py
@@ -7,5 +7,13 @@ verb = input("Enter a verb: ")
 adjective = input("Enter an adjective: ")
 adverb = input("Enter an adverb: ")
 
-story = f"Once upon a time, a {adjective} {noun} {verb} {adverb}."
+place = input("Enter a place: ")
+animal = input("Enter an animal: ")
+emotion = input("Enter an emotion: ")
+
+story = (
+	f"Once upon a time in {place}, a {adjective} {noun} decided to {verb} {adverb}. "
+	f"Along the way, it met a {animal} who was feeling very {emotion}. "
+	f"Together, they went on an adventure that changed their lives forever!"
+)
 print(story)
