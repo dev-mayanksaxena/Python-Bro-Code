c4d7cb3adbf21689e1ae86bb05202eea399d5a73
Mayank Saxena <dev.mayanksaxena@gmail.com>
2025-07-13 01:09:58 +0530

    Conditional Expression

---
+++ conditional_expression.py
@@ -0,0 +1,11 @@
+#CONDITIONAL EXPRESSION
+# A conditional expression is a way to evaluate a condition and return one of two values based on the result.
+# It is often used as a shorthand for an if-else statement.
+
+# Example usage:
+x = 5
+y = 10
+
+# Using a conditional expression
+result = "Yes" if x < y else "No"
+print(result)  # Yes
