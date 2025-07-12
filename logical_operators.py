3312c5890fdba027c2ffc1860b5bbbfb4af5f1e3
Mayank Saxena <dev.mayanksaxena@gmail.com>
2025-07-13 01:07:23 +0530

    Logical Operators

---
+++ logical_operators.py
@@ -0,0 +1,22 @@
+#LOGICAL OPERATORS
+# Logical operators are used to combine conditional statements
+# and return a boolean value (True or False).
+
+# The most common logical operators are:
+# 1. and
+# 2. or
+# 3. not
+
+# Example usage:
+x = 5
+y = 10
+z = 15
+
+# and operator
+print(x < y and y < z)  # True
+
+# or operator
+print(x < y or y > z)   # True
+
+# not operator
+print(not(x < y))        # False
