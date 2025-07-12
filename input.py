0441200d9388b1ea305ce3694fb0632c7999d06e
Mayank Saxena <dev.mayanksaxena@gmail.com>
2025-07-12 15:22:09 +0530

    Update input.py

--- input.py
+++ input.py
@@ -1,8 +1,8 @@
 #input() function is used to take input from the user
 # The input is always taken as a string, so type casting may be required for other datatypes.
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-height = float(input("Enter your height: "))
-is_student = bool(input("Are you a student? (True/False): "))
+age = int(input("Enter your age: ")) # Convert input to integer
+height = float(input("Enter your height: ")) # Convert input to float
+is_student = bool(input("Are you a student? (True/False): ")) # Convert input to boolean
 
-print(f"Name: {name}, Age: {age}, Height: {height}, Is Student: {is_student}")
+print(f"Name: {name}, Age: {age}, Height: {height}, Is Student: {is_student}")#Formatted string to display the input values
